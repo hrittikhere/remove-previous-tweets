afd214cc9f7f922243f017570f5452c3fa779160
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2022-12-27 06:01:24 +0000

    Add Bot

---
+++ remove.py
@@ -0,0 +1,24 @@
+import tweepy
+from time import sleep
+import os #for secrets
+
+
+## Set Enviroment Variables or replace the variables with your secret keys
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_KEY = os.environ['ACCESS_KEY']
+ACCESS_SECRET = os.environ['ACCESS_SECRET']
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+
+for status in tweepy.Cursor(api.user_timeline).items():
+            try:
+                api.destroy_status(status.id)
+                print("Deleted:", status.id)
+            except Exception:
+                print("Failed to delete:", status.id)
+ 
